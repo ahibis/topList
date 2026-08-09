57aa56213b26401baf44f1f252e37d5508f06a23
pokeprog <nikitonvg@list.ru>
2026-08-09 18:07:25 +0300

    build: new build

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,6 +49,9 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
   },
+  generate: {
+    routes: ["/old"]
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
